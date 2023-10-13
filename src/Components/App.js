bea3498625ba1fd90368a4d546c7a03116674423
Alike73 <alimzhanisla@gmail.com>
2023-10-12 18:48:49 -0700

    Created parallax-site

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Parallax } from './Parallax/Parallax';
+
+function App() {
+  return (
+    <div className="App">
+      <Parallax />
+    </div>
+  );
+}
+
+export default App;
